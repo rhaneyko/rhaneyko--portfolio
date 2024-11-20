be7c4ae00482b3e3dd3ac6a9b0528631d315f6e1
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-20 19:53:52 -0400

    Header Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Header from "./components/header";
+import React from "react";
+
+import './App.css'
+
+import Header from "./components/Header";
 import HomePage from "./components/Pages/HomePage";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <HomePage/>
+      <Header />
+      <HomePage />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import './styles.css'
+import "./styles.css";
 
 const Header = () => {
-    return (
-         <header className='header'>
-        <div className='logo'>
-            <p>logo</p>
-        </div>
-        <nav>
-          <ul className='nav-links'>
-            <li><a href='#home'>HOME</a></li>
-            <li><a href='#skills'>SKILLS</a></li>
-            <li><a href='#projects'>PROJETOS</a></li>
-          </ul>
-        </nav>
-        <button className='contact-button'>Contato</button>
-      </header>
-    )
-  }
-  
-  export default Header;
+  return (
+    <header className="header">
+      <div className="logo">
+        <p>logo</p>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a href="#home">HOME</a>
+          </li>
+          <li>
+            <a href="#skills">SKILLS</a>
+          </li>
+          <li>
+            <a href="#projects">PROJETOS</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="contact-button">Contato</button>
+    </header>
+  );
+};
+
+export default Header;

e27fdd41d69e02ed70e0447ee4c94ed17d3d3f87
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-20 19:44:10 -0400

    commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './styles.css'
+
+const Header = () => {
+    return (
+         <header className='header'>
+        <div className='logo'>
+            <p>logo</p>
+        </div>
+        <nav>
+          <ul className='nav-links'>
+            <li><a href='#home'>HOME</a></li>
+            <li><a href='#skills'>SKILLS</a></li>
+            <li><a href='#projects'>PROJETOS</a></li>
+          </ul>
+        </nav>
+        <button className='contact-button'>Contato</button>
+      </header>
+    )
+  }
+  
+  export default Header;
---
+++ src/components/Pages/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles.css'
+
+const HomePage = () => {
+  return (
+    <div className='Container'>
+        <h1>HomePage</h1>
+    </div>
+  )
+}
+
+
+export default HomePage;

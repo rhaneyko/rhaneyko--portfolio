d40d187babbe02595f6d81cda62a31bdb6c9bc6d
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-20 20:31:22 -0400

    HomePage commit

--- src/components/Pages/HomePage/index.js
+++ src/components/Pages/HomePage/index.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import './styles.css'
+import "./styles.css";
+import ProfileImage from "../../Assets/Images/profile__image.jpg";
 
 const HomePage = () => {
   return (
-    <div className='Container'>
-        <h1>HomePage</h1>
-    </div>
-  )
-}
+    <div className="container">
 
+      <div className="greetings">
+        <span className="greetings__text">Olá,</span>
+        <span className="greetings__text">Me chamo Rhaneyko.</span>
+        <span className="greetings__role">Desenvolvedor Frontend</span>
+      
+      <div className="buttons">
+        <button className="buttons__cv">Download CV</button>
+        <button className="buttons__linkedin">LinkedIn</button>
+      </div>
+      </div>
+
+
+      <div className="profile">
+        <img
+          className="profile__image"
+          src={ProfileImage}
+          alt="Foto de perfil"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;

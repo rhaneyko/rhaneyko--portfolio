17f2aed64664218dfd07d5b51e39a6351a1192c9
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-02 20:55:45 -0400

    skills page commit

--- src/components/Pages/SkillsPage/index.js
+++ src/components/Pages/SkillsPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import HtmlImg from "../../Assets/Images/html.png";
 import CssImg from "../../Assets/Images/css.png";
 import JsImg from "../../Assets/Images/JavaScript.png";
 import ReactImg from "../../Assets/Images/React.png";
@@ -11,18 +12,21 @@ const SkillsPage = () => {
     <div className="container__skills">
       <h1 className="skills__title">Skills</h1>
       <div className="skills__list">
-        <div className="skills__item">HTML</div>
         <div className="skills__item">
-          <img src={CssImg}  className="skills__item__img"/>
-          CSS
+          <img src={HtmlImg} className="skills__item__img" alt="HTML Icon" />
+          <span className="skills__item__name">HTML</span>
         </div>
         <div className="skills__item">
-          <img src={JsImg} className="skills__item__img"/>
-          JAVASCRIPT
+          <img src={CssImg} className="skills__item__img" alt="CSS Icon" />
+          <span className="skills__item__name">CSS</span>
         </div>
         <div className="skills__item">
-          <img src={ReactImg} className="skills__item__img"/>
-          REACT
+          <img src={JsImg} className="skills__item__img" alt="JavaScript Icon" />
+          <span className="skills__item__name">JAVASCRIPT</span>
+        </div>
+        <div className="skills__item">
+          <img src={ReactImg} className="skills__item__img" alt="React Icon" />
+          <span className="skills__item__name">REACT</span>
         </div>
       </div>
     </div>

a434cff3d1039fa27694d679af54d72a1c909071
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-09 22:46:46 -0400

    projects page commit

--- src/components/Pages/ProjectsPage/index.js
+++ src/components/Pages/ProjectsPage/index.js
@@ -2,26 +2,19 @@ import React, { useRef, useState } from "react";
 
 import "./styles.css";
 //import Carousel from "react-elastic-carousel";
-import projectsData from '../../Assets/Data/projectsData.json'
+import projectsData from "../../Assets/Data/projectsData.json";
 
 const ProjectsPage = () => {
-  const itemsPerPage = 1;
-  const items = [];
-  const ref = useRef(null);
-  const totalPages = Math.ceil(items.length / itemsPerPage);
-  let resetTimeout;
-
-  const [projets, setProjets] = useState(projectsData)
-
-
+  const [projets, setProjets] = useState(projectsData);
 
   return (
-    
-
     <div className="container__projectspage">
       <h1>ProjectsPage</h1>
-
-      <div className="project__card"></div>
+      <div className="card__list">
+        <div className="project__card"></div>
+        <div className="project__card"></div>
+        <div className="project__card"></div>
+      </div>
     </div>
   );
 };

fec06dcfd368347732e7ef5cbe18779923b29c43
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-11 22:33:10 -0400

    commit contact page

--- src/components/Pages/ContactPage/index.js
+++ src/components/Pages/ContactPage/index.js
@@ -4,36 +4,29 @@ import "./styles.css";
 
 const ContactPage = () => {
   return (
-    <div className="container__contactPage">
+    <div class="row">
       <h1>Contato</h1>
-   
+
       <div class="row input-container">
-			<div class="col-xs-12">
-				<div class="styled-input wide">
-					<input type="text" required />
-					<label>Name</label> 
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-12">
-				<div class="styled-input">
-					<input type="text" required />
-					<label>Email</label> 
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-12">
-				<div class="styled-input">
-					<input type="text" required />
-					<label>Phone Number</label> 
-				</div>
-			</div>
-			<div class="col-xs-12">
-				<div class="styled-input wide">
-					<textarea required></textarea>
-					<label>Message</label>
-				</div>
-			</div>
-      
-    </div>
+        <div class="styled-input wide">
+          <input type="text" required />
+          <label>Name</label>
+        </div>
+
+        <div class="styled-input">
+          <input type="text" required />
+          <label>Email</label>
+        </div>
+
+        <div class="styled-input">
+          <input type="text" required />
+          <label>Phone Number</label>
+        </div>
+        <div class="styled-input wide">
+          <textarea required></textarea>
+          <label>Message</label>
+        </div>
+      </div>
     </div>
   );
 };

1b66c1339d2f72610fa49824e6255db47d4b74a4
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-12 21:55:31 -0400

    commit header scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,30 +5,36 @@ import { NavHashLink } from "react-router-hash-link";
 
 import "./styles.css";
 
+const scrollWithOffset = (el) => {
+  const yOffset = -70;
+  const yCoordinate =
+    el.getBoundingClientRect().top + window.scrollY + yOffset;
+  window.scrollTo({ top: yCoordinate, behavior: "smooth" });
+};
+
 const Header = () => {
   return (
     <header className="header">
       <Router>
         <div className="nav-links">
-
-        <div className="logo" smooth to="#home">
-          <p>logo</p>
-        </div>
+          <div className="logo" smooth to="#home">
+            <p>logo</p>
+          </div>
           <ul>
-            <NavHashLink smooth to="#home">
+            <NavHashLink smooth to="#home" scroll={scrollWithOffset}>
               HOME
             </NavHashLink>
-            <NavHashLink smooth to="#skills">
+            <NavHashLink smooth to="#skills" scroll={scrollWithOffset}>
               SKILLS
             </NavHashLink>
-            <NavHashLink smooth to="#projects" >
+            <NavHashLink smooth to="#projects" scroll={scrollWithOffset}>
               PROJETOS
             </NavHashLink>
-            <NavHashLink smooth to="#contact">
+            <NavHashLink smooth to="#contact" scroll={scrollWithOffset}>
               CONTATOS
             </NavHashLink>
           </ul>
-        <button className="contact-button">Contato</button>
+          <button className="contact-button">Contato</button>
         </div>
       </Router>
     </header>

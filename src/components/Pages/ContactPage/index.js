2ed22b355a17b9f8a7cbf20f518a3bb9de6dae7d
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-12 22:18:20 -0400

    commit contact page

--- src/components/Pages/ContactPage/index.js
+++ src/components/Pages/ContactPage/index.js
@@ -1,22 +1,33 @@
 import React from "react";
 
-import "./styles.css"
+import "./styles.css";
 
 const ContactPage = () => {
-    return (
-       <div className="container__contactPage" id="contact">
-        <h1>Contato</h1>
-        <form>
-            <label for="name">Nome</label>
-            <input type="text" placeholder="Digite seu nome"/>
-            <label for="email">E-mail</label>
-            <input type="email" placeholder="Digite seu e-mail"/>
-            
-            <textarea placeholder="Digite sua mensagem..."/>
-        </form>
-       </div>
+  return (
+    <div className="container__contactPage" id="contact">
+      <h1>Contato</h1>
+      <form className="row input-container">
+        <div className="styled-input wide">
+          <input type="text" />
+          <label for="name">Nome</label>
+        </div>
+        <div className="styled-input wide">
+          <input type="email" />
+          <label for="email">E-mail</label>
+        </div>
+        <div className="styled-input wide">
+          <input type="text" />
+          <label>Telefone</label>
+        </div>
+        <div className="styled-input wide">
+          <textarea required></textarea>
+          <label>Mensagem</label>
+        </div>
 
-    )
-}
+        <button className="submit-button">Enviar</button>
+      </form>
+    </div>
+  );
+};
 
 export default ContactPage;

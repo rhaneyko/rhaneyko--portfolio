fce29f7e7bdd2e56e639c5deb57bd7296175b91d
rhaneyko <rhaneykohonorio@gmail.com>
2024-12-14 12:09:44 -0400

    Estilização da página de contato.

--- src/components/Pages/ContactPage/index.js
+++ src/components/Pages/ContactPage/index.js
@@ -6,26 +6,29 @@ const ContactPage = () => {
   return (
     <div className="container__contactPage" id="contact">
       <h1>Contato</h1>
-      <form className="row input-container">
+      <div className="row input-container">
         <div className="styled-input wide">
-          <input type="text" />
-          <label for="name">Nome</label>
+          <input type="text" required />
+          <label>Nome</label>
         </div>
-        <div className="styled-input wide">
-          <input type="email" />
-          <label for="email">E-mail</label>
+
+        <div className="styled-input">
+          <input type="text" required />
+          <label>E-mail</label>
         </div>
-        <div className="styled-input wide">
-          <input type="text" />
-          <label>Telefone</label>
+        <div>
+          <div className="styled-input">
+            <input type="text" required />
+            <label>Telefone</label>
+          </div>
         </div>
-        <div className="styled-input wide">
+
+        <div class="styled-input wide">
           <textarea required></textarea>
           <label>Mensagem</label>
         </div>
-
-        <button className="submit-button">Enviar</button>
-      </form>
+        <button className=" submit-button">Enviar</button>
+      </div>
     </div>
   );
 };
